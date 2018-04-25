74e601b35cccb2e598a7d2e0777ed8fcbd1c7230
kwanj-k <mwangikelvin23@gmail.com>
2018-04-25 08:55:59 +0300

    [Bug] Fix app.py [(Finishes|Fixes|Deivers) #157075131]

--- api/app.py
+++ api/app.py
@@ -1,9 +1,25 @@
-from flask import Flask, request, jsonify, make_response
-import json 
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+# meals = [
+#     {
+#         'id': 1,
+#         'meal_name': 'Lunch',
+#         'description': 'Milk, Cheese, Pizza, Fruit, Tylenol', 
+#         'done': False
+#     },
+#     {
+#         'id': 2,
+#         'meal_name': 'Lunch',
+#         'menu': 'Milk, Cheese, Pizza, Fruit, Tylenol', 
+#         'done': False
+#     }
+# ]
 
+# @app.route('/api/v1/meals', methods=['GET'])
+# def get_meals():
+#     return jsonify({'meals': meals})
 
 if __name__ == '__main__':
-    unittest.main()
+    app.run(debug=True)
