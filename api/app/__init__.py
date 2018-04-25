64eca3a96474896c0ebb7388c9bc67a85c7428b0
kwanj-k <mwangikelvin23@gmail.com>
2018-04-25 15:25:08 +0300

    [bug] Fix test error

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
-import json 
+# from flask_restful import Api
+# from resources.meal import Meal,MealList
+
 
 app = Flask(__name__)
 
 
 
-if __name__ == '__main__':
-    app.run()
+# api.add_resource(MealList, '/api/v1/meals')
+# api.add_resource(Meal, '/api/v1/meals/<meal_name>')
+
