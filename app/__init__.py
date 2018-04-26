7f285b19007f08fc5ee3c101ae9e5f79cd9d471a
kwanj-k <mwangikelvin23@gmail.com>
2018-04-26 03:45:00 +0300

    [ft] Caterer can add and get meals

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
-# from flask_restful import Api
-# from resources.meal import Meal,MealList
-
 
 app = Flask(__name__)
 
+meals = [
+    {
+        'id': 1,
+        'name': u'Breakfast',
+    },
+    {
+        'id': 2,
+        'name': u'Lunch',
+       
+    }
+]
 
 
-# api.add_resource(MealList, '/api/v1/meals')
-# api.add_resource(Meal, '/api/v1/meals/<meal_name>')
+
 
