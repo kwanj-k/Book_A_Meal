0c00c9d4684c5b753f6e141d8cf40fd6f0ae3f0c
kwanj-k <mwangikelvin23@gmail.com>
2018-05-06 08:10:52 +0300

    [FIXES] travis

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Book_a_Meal',
+      version='0.0.1',
+      description='mealbooking service',
+      author='',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
