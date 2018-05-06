6de13d0a96ee7819b506d83de01a7c6e2fa77b81
kwanj-k <mwangikelvin23@gmail.com>
2018-05-06 07:45:15 +0300

    [FIXES] travis

---
+++ .travis.sh
@@ -0,0 +1,9 @@
+export MODE='testing'
+export FLASK_APP='manage.py'
+export FLASK_DEBUG=1
+export SECRET_KEY="very-long-strings"
+export TEST_DATABASE_URI="postgres://postgres@localhost/travis_test"
+
+export DEFAULT_ADMIN_USERNAME='kelvin'
+export DEFAULT_ADMIN_PASSWORD='kwanjkay'
+export DEFAULT_ADMIN_EMAIL='mwangikelvin23@gmail.com'
